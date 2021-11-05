a20bf3f7831bbf911c91acab13a334170673b846
allison-alves <allison.andre2@gmail.com>
2021-11-05 15:11:59 -0300

    Add automaçao do cadastro e json com os dados do login

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,7 +1,20 @@
 context('Task it', () => {
-    it('Sign Up for new user', () => {
-        cy.get(`a[data-target='signupbox']`).click();
-        cy.get('input[name="name"]').type('')
+    //array que contem os dados do teste e a localizacao da pasta onde estao os dados.
+    const users = require('../fixtures/users')
+    //for que irá buscar os dados de acordo com o idex e adiconar nos cy.get
+    users.forEach((item,index) => {
+        it(`${index+1} - Sign Up for new ser`, () => {
+            cy.visit('http://www.juliodelima.com.br/taskit/');
+            cy.get('a[data-target="signupbox"]').click();
+            cy.get('input[name="name"]').type(item.name);
+            cy.get(`input[placeholder="It's time to create a login"]`).type(item.login);
+            cy.get(`input[placeholder='Take care, it need to be remembered']`).type(item.password);
+            cy.get('#signupbox > .modal-footer > .modal-action').click()
+    
+            //verifica se a mensagem de erro aparece corretamente.
+            //cy.get('.toast').should('be.visible').and('contain', 'Someone choose this login before, please pick another!')
+        });
+        
     });
-
 });
+
